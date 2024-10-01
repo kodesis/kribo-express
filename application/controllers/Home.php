9ed01c51effe54efd87f713a76ec306a90b1f143
adpratama <adpratama.adp@gmail.com>
2024-10-01 12:17:19 +0700

    update: penyesuaian google recaptcha untuk hosting

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -60,51 +60,52 @@ class Home extends CI_Controller
 		if (!empty($nomor_resi)) {
 			$this->form_validation->set_rules('g-recaptcha-response', 'reCAPTCHA', 'required');
 
-			// if ($this->form_validation->run() == FALSE) {
-			// 	$this->session->set_flashdata('message', 'reCAPTCHA is required.');
-			// 	redirect('home/track');
-			// } else {
-			// 	$recaptchaResponse = $this->input->post('g-recaptcha-response');
-			// 	$secretKey = '6Le8ZkcqAAAAACRQtxwa7SSZpcyETjp_gWkVmLBE';
-			// 	$userIP = $this->input->ip_address();
+			if ($this->form_validation->run() == FALSE) {
+				$this->session->set_flashdata('message', 'reCAPTCHA is required.');
+				redirect('home/track');
+			} else {
+				$recaptchaResponse = $this->input->post('g-recaptcha-response');
+				// $secretKey = '6Le8ZkcqAAAAACRQtxwa7SSZpcyETjp_gWkVmLBE'; // Local
+				$secretKey = '6LcrQFQqAAAAAKO2oNf5Gx-8MOR1vExhoD0oiDVY'; // Hosting
+				$userIP = $this->input->ip_address();
 
-			// 	$url = 'https://www.google.com/recaptcha/api/siteverify';
+				$url = 'https://www.google.com/recaptcha/api/siteverify';
 
-			// 	$data = [
-			// 		'secret' => $secretKey,
-			// 		'response' => $recaptchaResponse,
-			// 		'remoteip' => $userIP
-			// 	];
+				$data = [
+					'secret' => $secretKey,
+					'response' => $recaptchaResponse,
+					'remoteip' => $userIP
+				];
 
-			// 	$options = [
-			// 		'http' => [
-			// 			'header' => 'Content-type: application/x-www-form-urlencoded',
-			// 			'method' => 'POST',
-			// 			'content' => http_build_query($data)
-			// 		]
-			// 	];
+				$options = [
+					'http' => [
+						'header' => 'Content-type: application/x-www-form-urlencoded',
+						'method' => 'POST',
+						'content' => http_build_query($data)
+					]
+				];
 
-			// 	$context = stream_context_create($options);
-			// 	$result = file_get_contents($url, false, $context);
-			// 	$resultData = json_decode($result);
+				$context = stream_context_create($options);
+				$result = file_get_contents($url, false, $context);
+				$resultData = json_decode($result);
 
-			// 	if ($resultData->success) {
+				if ($resultData->success) {
 
-			// 		$nomor_resi = $this->input->post('nomor_resi');
+					$nomor_resi = $this->input->post('nomor_resi');
 
-			// 		$cek_resi = $this->M_Booking->cekItemAwb($nomor_resi);
+					$cek_resi = $this->M_Booking->cekItemAwb($nomor_resi);
 
-			// 		if ($cek_resi) {
-			// 			$resi = $this->M_Booking->getItemAwb($nomor_resi);
+					if ($cek_resi) {
+						$resi = $this->M_Booking->getItemAwb($nomor_resi);
 
-			// 			$this->session->set_flashdata('message', "Nomor resi  $nomor_resi ditemukan!");
-			// 		} else {
-			// 			$this->session->set_flashdata('message', "Nomor resi  $nomor_resi tidak ditemukan!");
-			// 		}
-			// 	} else {
-			// 		$this->session->set_flashdata('message', 'Unable to verify reCAPTCHA. Please try again later.');
-			// 	}
-			// }
+						$this->session->set_flashdata('message', "Nomor resi  $nomor_resi ditemukan!");
+					} else {
+						$this->session->set_flashdata('message', "Nomor resi  $nomor_resi tidak ditemukan!");
+					}
+				} else {
+					$this->session->set_flashdata('message', 'Unable to verify reCAPTCHA. Please try again later.');
+				}
+			}
 
 			$nomor_resi = $this->input->post('nomor_resi');
 
--- application/views/pages/front/home/v_track.php
+++ application/views/pages/front/home/v_track.php
@@ -31,7 +31,11 @@
 							</div>
 							<div class="col-12">
 								<!-- Google reCAPTCHA v2 widget -->
-								<div class="g-recaptcha" data-sitekey="6Le8ZkcqAAAAAMaPLJPvTfEmiAJxPdh-wBXJmoma"></div>
+								<!-- Local -->
+								<!-- <div class="g-recaptcha" data-sitekey="6Le8ZkcqAAAAAMaPLJPvTfEmiAJxPdh-wBXJmoma"></div> -->
+
+								<!-- Hosting -->
+								<div class="g-recaptcha" data-sitekey="6LcrQFQqAAAAAEa0DkcI1dUU0mxEt48SZ6LXMiic"></div>
 							</div>
 							<div class="col-12">
 								<button class="btn btn-primary w-100 py-3" type="submit">Lacak Sekarang</button>
